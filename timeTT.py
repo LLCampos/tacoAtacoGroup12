7501b04105f0ef566803108560565dc61ccd94e4
LLCampos <luis1212@sapo.pt>
2015-12-03 20:47:07 +0000

    updated 'timeTT'
    Patricia's implementation

--- timeTT.py
+++ timeTT.py
@@ -5,32 +5,32 @@
 # 43071 Ana Patrícia Dos Santos Abrantes
 # 43134 Luís Filipe Leal Campos
 
+# Especificações não dizem supostamente apenas o que a função faz e não como o faz?
+# Falar de driver?
 
 
 def hourToInt(time):
-    """Splits time using : as a delimiter and obtain a list of substrings of time. 
-    Then transforms substrings into integers and gives the hour of any time.
+    """Gives the hour of any time.
     Requires: time is of type string.
     Ensures: The transformation of the string time into integer and gives
-    the zero position.
+    the zeroth position.
     """
     t = time.split(":")
     return int(t[0])
-    
 
 
 def minutesToInt(time):
-    """Splits time using : as a delimiter and obtain a list of substrings of time.
-    Then transforms substrings into integers and gives the minutes of any time.
+    """Gives the minutes of any time.
     Requires: time is of type string.
     Ensures: The transformation of the string time into integer and gives
-    the one position.
+    the first position.
     """
     t = time.split(":")
     return int(t[1])
 
+
 def intToTime(hour, minutes):
-    """ Transforms hour and minutes into strings and gives the argument Time as a integer's type.
+    """ Gives time as a string.
     Requires: hour and minutes are of the type integer.
     Ensures: The transformation of the integers hour and minutes into strings.
     """
@@ -39,24 +39,19 @@ def intToTime(hour, minutes):
 
     if hour < 10:
         h = "0" + h
-    else:
-        h = h
 
     if minutes < 10:
         m = "0" + m
-    else:
-        m = m
 
     return h + ":" + m
 
 
-
 def add(time1, time2):
-    """ Adds a quantity of time to the working time already performed by the driver.
-    Requires: time1 and time2 as strings.
-    Ensures: A string with the accumulative time performed by the driver.
+    """ Adds a quantity of time to the working time elapsed.
+    Requires: time1 and time2 as strings with "hh:mm" format.
+    Ensures: A string with the accumulative time elapsed.
     """
-    t1Hour = hourToInt(time1) 
+    t1Hour = hourToInt(time1)
     t1Minutes = minutesToInt(time1)
     t2Hour = hourToInt(time2)
     t2Minutes = minutesToInt(time2)
@@ -70,10 +65,9 @@ def add(time1, time2):
     return intToTime(t1H, t1M)
 
 
-
 def diff(time1, time2):
-    """Removes a quantity of time to the working time already performed by the driver.
-    Requires: time1 and time2 as strings.
+    """Removes a quantity of time to the working time time elapsed.
+    Requires: time1 and time2 as strings with "hh:mm" format.
     Ensures: A string with the difference between time1 and time2.
     """
     t1Hour = hourToInt(time1)
