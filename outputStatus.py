2a57cdd1bf2e78cf6300df6a0141b339cc57954f
LLCampos <luis1212@sapo.pt>
2015-12-03 15:25:04 +0000

    completed the module

--- outputStatus.py
+++ outputStatus.py
@@ -5,14 +5,22 @@
 # 43071 Ana Patrícia Dos Santos Abrantes
 # 43134 Luís Filipe Leal Campos
 
-'''
+
+def changeFormatTime(period):
+    #recebo isto 1921 e quero 19:00 - 21:00 esta tudo em str
+    H1 = period[0:2]
+    H2 = period[2:4]
+
+    return H1 + ":00 - " + H2 + ":00"
+
+
 def makeHeader(company, date, p, type):
     """Constructs a header for a file
 
     Requires:
     company is a string with the name of the company
     date is a string with a date in the format "DD:MM:YYYY"
-    p is a string with the format "HH:MM - HH:MM" indicating a
+    p is a string with the format "HHHH" indicating a
     2 hour period
     type is one of the following strings: "Reservations",
     "Services", "Drivers", "Vehicles"
@@ -21,7 +29,9 @@ def makeHeader(company, date, p, type):
     the examples given in the general specification (ommited here for the
     sake of readability)
     """
-'''
+    header = "Company:," + company + ",Day:," + date + ",Period:," + changeFormatTime(p) + "," + type + ":"
+
+    return header
 
 
 def writeServicesFile(services_p, file_name_p, header_p):
@@ -42,5 +52,25 @@ def writeServicesFile(services_p, file_name_p, header_p):
     the general specification (omitted here for the sake of readability);
     in the listing in this file keep the ordering of services in services_p.
     """
+
+    f = open(file_name_p + '.txt', 'w')
+
+    h = header_p.split(',')
+
+    for line in h:
+        f.write(line + '\n')
+
+    for line in services_p:
+        line = ', '.join(line)
+        f.write(line + '\n')
+
+    f.close()
+
+
+
+
+
+
+
 
 
