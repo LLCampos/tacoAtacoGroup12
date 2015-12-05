cfed2ea43bcfd3e18a12257565c5afc4964a4f6c
LLCampos <luis1212@sapo.pt>
2015-12-05 09:50:10 +0000

    add raising of errors

--- tukTuk.py
+++ tukTuk.py
@@ -9,7 +9,7 @@ import sys
 from consultStatus import *
 from planning import updateServices
 from outputStatus import writeServicesFile
-from headerRelated import createNewHeader
+from headerRelated import createNewHeader, getHeader
 
 nextPeriod = sys.argv[1]
 driversFileName = sys.argv[2]
@@ -18,8 +18,45 @@ servicesFileName = sys.argv[4]
 reservationsFileName = sys.argv[5]
 
 
+def checkPreConditions(nextPeriod, driversFileName, vehiclesFileName,
+                       servicesFileName, reservationsFileName):
+
+    headerDrivers = getHeader(driversFileName)
+    headerVehicles = getHeader(vehiclesFileName)
+    headerServices = getHeader(servicesFileName)
+    headerReservations = getHeader(reservationsFileName)
+
+    # the files whose names are driversFileName, vehiclesFileName, servicesFileName and reservationsFileName
+    # concern the same company and the same day;
+    if not (headerDrivers[INDEXCompany:INDEXDate + 1] == headerVehicles[INDEXCompany:INDEXDate + 1] == headerServices[INDEXCompany:INDEXDate + 1] == headerReservations[INDEXCompany:INDEXDate + 1]):
+        return False
+
+    # the file whose name is reservationsFileName concerns the period indicated by nextPeriod
+    elif False:
+        return False
+
+    # the files whose names are driversFileName, vehiclesFileName, servicesFileName concern the period
+    # immediately preceding the period indicated by nextPeriod;
+    elif False:
+        return False
+
+    # the file name reservationsFileName ends (before the .txt extension) with
+    # the string nextPeriod;
+    elif reservationsFileName[-8:-4] != nextPeriod:
+        return False
+
+    # the file names driversFileName, vehiclesFileName and servicesFileName
+    # end (before their .txt extension) with the string representing
+    # the period immediately preceding the one indicated by nextPeriod,
+    # from the set 0709, 0911, ..., 1719;
+    elif False:
+        return False
+    else:
+        return True
+
+
 # PARA A PATRICIA FAZER
-def update(nextPeriod, driversFileName, vehiclesFileName, \
+def update(nextPeriod, driversFileName, vehiclesFileName,
            servicesFileName, reservationsFileName):
     """Obtains the planning for a period of activity.
 
@@ -55,20 +92,26 @@ def update(nextPeriod, driversFileName, vehiclesFileName, \
     the name of that file is outputXXYY.txt where XXYY represents
     the nextPeriod.
     """
-    outputFileName = 'output' + nextPeriod
 
-    header = createNewHeader(servicesFileName, nextPeriod)
+    if checkPreConditions(nextPeriod, driversFileName, vehiclesFileName, servicesFileName, reservationsFileName):
 
-    drivers = readDriversFile(driversFileName)
-    vehicles = readVehiclesFile(vehiclesFileName)
-    services = readServicesFile(servicesFileName)
-    reservations = readReservationsFile(reservationsFileName)
+        outputFileName = 'output' + nextPeriod
 
-    waiting4Services = waiting4ServicesList(drivers, vehicles, services)
+        header = createNewHeader(servicesFileName, nextPeriod)
 
-    newServices = updateServices(reservations, waiting4Services)
+        drivers = readDriversFile(driversFileName)
+        vehicles = readVehiclesFile(vehiclesFileName)
+        services = readServicesFile(servicesFileName)
+        reservations = readReservationsFile(reservationsFileName)
 
-    writeServicesFile(newServices, outputFileName, header)
+        waiting4Services = waiting4ServicesList(drivers, vehicles, services)
+
+        newServices = updateServices(reservations, waiting4Services)
+
+        writeServicesFile(newServices, outputFileName, header)
+
+    else:
+        raise IOError('File names and/or headers not consistent.')
 
 update(nextPeriod, driversFileName, vehiclesFileName, servicesFileName, reservationsFileName)
 
